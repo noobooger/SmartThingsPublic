c71c4894c30242aeb2cb29191be4d57f964a2ca6
noobooger <josephried@gmail.com>
2016-12-02 11:35:17 -0500

    Update smart-humidifier.groovy

--- smartapps/sheikhsphere/smart-humidifier.src/smart-humidifier.groovy
+++ smartapps/sheikhsphere/smart-humidifier.src/smart-humidifier.groovy
@@ -14,7 +14,7 @@
  *
  */
 definition(
-    name: "Smart Humidifier",
+    name: "Custom Smart Humidifier",
     namespace: "Sheikhsphere",
     author: "Sheikh Dawood",
     description: "Turn on/off humidifier based on relative humidity from a sensor.",
@@ -62,9 +62,16 @@ def humidityHandler(evt) {
     def humidityLow1 = humidityLow 
 	def mySwitch = settings.switch1
 
-	if (currentHumidity >= humidityHigh1) {
+	if (currentHumidity > humidityHigh1) {
 		log.debug "Checking how long the humidity sensor has been reporting >= $humidityHigh1"
 
+        if (state.lastStatus != "off") {
+            log.debug "Humidity Rose Above $humidityHigh1:  sending SMS and deactivating $mySwitch"
+            send("${humiditySensor1.label} sensed high humidity level of ${evt.value}, turning off ${switch1.label}")
+            switch1?.off()
+            state.lastStatus = "off"
+        }
+/*
 		// Don't send a continuous stream of text messages
 		def deltaMinutes = 10 
 		def timeAgo = new Date(now() - (1000 * 60 * deltaMinutes).toLong())
@@ -83,12 +90,13 @@ def humidityHandler(evt) {
                 state.lastStatus = "off"
             }
 		}
+        */
 	}
-    else if (currentHumidity <= humidityLow1) {
+    else if (currentHumidity < humidityLow1) {
 		log.debug "Checking how long the humidity sensor has been reporting <= $humidityLow1"
 
 		// Don't send a continuous stream of text messages
-		def deltaMinutes = 10 
+		/*def deltaMinutes = 10 
 		def timeAgo = new Date(now() - (1000 * 60 * deltaMinutes).toLong())
 		def recentEvents = humiditySensor1.eventsSince(timeAgo)
 		log.trace "Found ${recentEvents?.size() ?: 0} events in the last $deltaMinutes minutes"
@@ -101,10 +109,17 @@ def humidityHandler(evt) {
         	if (state.lastStatus != "on") {
                 log.debug "Humidity Dropped Below $humidityLow1:  sending SMS and activating $mySwitch"
                 send("${humiditySensor1.label} sensed low humidity level of ${evt.value}, turning on ${switch1.label}")
-                switch1?.on()
-                state.lastStatus = "on"
+	            switch1?.on()
+    	        state.lastStatus = "on"
             }
-		}
+		}*/
+        
+        if (state.lastStatus != "on") {
+            log.debug "Humidity Dropped Below $humidityLow1:  sending SMS and activating $mySwitch"
+            send("${humiditySensor1.label} sensed low humidity level of ${evt.value}, turning on ${switch1.label}")
+            switch1?.on()
+            state.lastStatus = "on"
+        }
 	}
     else {
     	//log.debug "Humidity remained in threshold:  sending SMS to $phone1 and activating $mySwitch"
